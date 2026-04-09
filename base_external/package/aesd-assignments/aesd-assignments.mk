8d0047e89109f2296f5583d150c18643703bfbf4
Octavio Delgadillo <octidr@gmail.com>
2026-04-09 22:26:40 +0200

    Commit for assignment 9

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION =  059a4570360c9d5b19e9d72ac381dd21fab7228a
+AESD_ASSIGNMENTS_VERSION =  1e511995519db84569be7e3b9ee5d523b66cc4e8
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
